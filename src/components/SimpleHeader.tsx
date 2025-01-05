a2b490f04eec4266b8c04a1ecf389a9bb8c9b541
svenschu <sve.sch@icloud.com>
2025-01-05 15:26:12 +0100

    performance

--- src/components/SimpleHeader.tsx
+++ src/components/SimpleHeader.tsx
@@ -1,12 +1,16 @@
-// src/SimpleHeader.tsx
 import React from "react";
 
 const SimpleHeader: React.FC = ({children}: any) => {
     return (
-        <header className="bg-brand-aqua-50 p-4">
-            <div className="max-w-48">
-                {children}
-            </div>
+        <header className="bg-brand-aqua-50 absolute inset-x-0 top-0 z-50">
+            <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="/" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        {children}
+                    </a>
+                </div>
+            </nav>
         </header>
     );
 };
