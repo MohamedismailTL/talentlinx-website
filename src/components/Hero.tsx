7d77ee41c6784c3598a77aee65f3d4e7533aa211
svenschu <sve.sch@icloud.com>
2025-01-12 22:21:09 +0100

    Fixed mobile menu

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -49,16 +49,8 @@ export default function Example({content, metadata, children}: any) {
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                     <div className="fixed inset-0 z-50"/>
                     <DialogPanel
-                        className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-brand-primary-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-brand-primary-700">
+                        className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-brand-aqua-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-brand-primary-700">
                         <div className="flex items-center justify-between">
-                            <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
-                                <img
-                                    alt=""
-                                    src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                                    className="h-8 w-auto"
-                                />
-                            </a>
                             <button
                                 type="button"
                                 onClick={() => setMobileMenuOpen(false)}
